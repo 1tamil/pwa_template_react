3afeaeebaf2f7f330ec0ccb004ae884d2d9c8b2e
1tamil <tzrajesh7@outlook.com>
2020-07-18 12:18:57 +0000

    template_update

---
+++ precache-manifest.84791ca2863d18218dd799e4cbd2f66c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774cc9e547a2d4430e1d8ca31f6b8f73",
+    "url": "/pwa_template_react/index.html"
+  },
+  {
+    "revision": "b4e9ee127b0a7e1ecca8",
+    "url": "/pwa_template_react/static/css/main.a99ab8d4.chunk.css"
+  },
+  {
+    "revision": "20a2bd10b2690335e46b",
+    "url": "/pwa_template_react/static/js/2.4869306a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pwa_template_react/static/js/2.4869306a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e9ee127b0a7e1ecca8",
+    "url": "/pwa_template_react/static/js/main.1ab48e14.chunk.js"
+  },
+  {
+    "revision": "07e7a0b76b9b3fd2caea",
+    "url": "/pwa_template_react/static/js/runtime-main.b232df1c.js"
+  }
+]);
